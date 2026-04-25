56fb8a5787255147bbb738fd06a96d474b6c4b11
Omid Kandelusy <omidkandelusy@gmail.com>
2026-04-25 16:49:24 +0200

    feat: populates an initial uart interface

---
+++ uart/uart_interface.c
@@ -0,0 +1,50 @@
+#include "uart_interface.h"
+
+
+// ========================================================================================
+// variable definitions
+
+/** uart peripheral driver configuration variables */
+static UART2_Handle handle;
+static UART2_Params params;
+static int read_bytes = 0;
+static uint8_t read_buff[READ_BUFF_SIZE]
+
+/** user callback registery for the read events */
+static uart_read_cb user_cb = NULL;
+// ========================================================================================
+// callbacks and helper functions
+
+void uart_callback(UART2_Handle handle, void *buffer, size_t count,
+                         void *userArg, int_fast16_t status){
+    if (user_cb){
+        user_cb(buffer, count);
+    }
+}
+
+// ========================================================================================
+// main functions
+
+int uart_init(uart_read_cb cb){
+    if (!cb) return -1;
+
+    UART2_Params_init(&params);
+    params.baudRate = BAUDRATE;
+    params.writeMode = UART2_Mode_BLOCKING;
+    params.readMode = UART2_Mode_CALLBACK;
+    params.readCallback = uart_callback;
+    params.readReturnMode = UART2_ReadReturnMode_PARTIAL;
+
+
+    handle = UART2_open(CONFIG_UART2_0, &params);
+    if (!handle){
+        return -1;
+    }
+
+    UART2_read(handle, read_buff, READ_BUFF_SIZE, &read_bytes);
+
+    const char init_msg[] = "\n\r uart interface is initialized \n\r";
+    UART2_write(handle, init_msg, sizeof(init_msg), NULL);
+
+    return 0;
+};
---
+++ uart/uart_interface.h
@@ -0,0 +1,34 @@
+#ifndef UART_INTERFACE_H_GUARD
+#define UART_INTERFACE_H_GUARD
+// ========================================================================================
+
+/** standard C header files */
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+
+/** TI's driver and configuration for uart driver */
+#include "ti_drivers_config.h"
+#include <ti/drivers/UART2.h>
+// ========================================================================================
+// typedefs, macros and constants
+
+/** baudrate used by the uart peripheral for decoding the pulses on the line */
+#define BAUDRATE 115200
+
+/** the size of the buffer that holds the bytes read from the line */
+#define READ_BUFF_SIZE 64
+
+// ========================================================================================
+// APIs
+
+/**
+ * @brief initializes the uart interface for the application
+ * 
+ * @param [in] cb the read callback that application can register 
+ * 
+ * @return SUCCESS if succeeds; negative error code otherwise.
+ */
+int uart_init(uart_read_cb cb);
+
+#endif
